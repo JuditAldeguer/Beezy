9ac3bf1ea8843730e4d567619b7ef0d1074499ef
JuditAldeguer <jud.vicens@hotmail.es>
2022-01-07 21:29:26 +0100

    update Data file

--- src/types/Data.tsx
+++ src/types/Data.tsx
@@ -11,21 +11,3 @@ export interface dataEl {
   category: string;
   better_call_saul_appearance: never[];
 }
-
-// export interface noData {
-//   char_id: number;
-//   name: string;
-//   birthday?: string;
-//   occupation?: string[];
-//   img: string;
-//   status?: string;
-//   nickname: string;
-//   appearance?: number[];
-//   portrayed?: string;
-//   category?: string;
-//   better_call_saul_appearance?: never[];
-// }
-
-// export interface dataArr {
-//   data: dataEl[];
-// }
